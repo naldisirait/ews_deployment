36a2519e898c19f31bc4ed3c5befb0ee0f2f1f16
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-09-19 22:08:25 +0700

    add test api

---
+++ test_api.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Define a simple route to get a greeting
+@app.get("/greet")
+def greet(name: str = "World"):
+    return {"message": f"Hello, {name}!"}
