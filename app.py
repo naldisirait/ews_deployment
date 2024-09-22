88ea0e5de0b44a595eb83717b9c4eef9704908af
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-09-22 08:41:59 +0700

    add

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def get_current_datetime():
     # Get the current date and time
     now = datetime.now()
     # Format the date and time as YYYY-MM-DD HH:MM:SS
-    return now.strftime("%Y-%m-%d %H:%M")
+    return now.strftime("%Y-%m-%d %H:%M:%S")
 
 # Define input data model
 class InputData(BaseModel):
@@ -34,6 +34,7 @@ app = FastAPI()
 def do_prediction():
     start_run_pred = get_current_datetime()
 
+    #1. Define all constants and load models
     hours = 144
     jumlah_subdas = 114
     input_size_ml1 = hours * jumlah_subdas #jumlah jam dikali jumlah subdas
@@ -44,26 +45,23 @@ def do_prediction():
     output_size_ml2 = 3078 * 2019 #rows x columns 
     model_ml2 = load_model_ml2(input_size=input_size_ml2, output_size=output_size_ml2)
 
-    #input_ml1 = to_tensor(np.random.rand(1,16416))
+    #2. Ingest Data input
     path_config_stas_to_grid = "/opt/ews/ews_deployment/configs/configuration of stasiun to grid.json"
     path_config_grid_to_subdas = "/opt/ews/ews_deployment/configs/configuration of grid to subdas.json"
 
     ingested_data_name = "Stasiun"
     ingested_data = get_precipitation_from_big_lake(hours)
 
+    #3. Inference ML1
     all_grided_data, dates, input_ml1 =  get_input_ml1(ingested_data,
                                                    ingested_data_name,
                                                    path_config_stas_to_grid,
                                                    path_config_grid_to_subdas)
     
     all_grided_data = np.array(all_grided_data)
-
     output_ml1 = inference_model(model_ml1,input_ml1)
-    print(output_ml1.shape)
-
-    # #Convert output ml1 to dict
-    dates, dict_output_ml1 = output_ml1_to_dict(dates=dates, output_ml1=output_ml1[0,:].tolist(), precipitation=all_grided_data.tolist())
     
+    #4. Inference ML2
     output_ml1 = output_ml1[:,-input_size_ml2:]
     input_ml2 = np.expand_dims(output_ml1, axis=-1)
     input_ml2 = to_tensor(input_ml2)
@@ -71,15 +69,13 @@ def do_prediction():
     output_ml2 = output_ml2[0,:].reshape(3078,2019)
     print(output_ml2.shape)
 
+    #5. Bundle the Output
+    #Convert output ml1 to dict
+    dates, dict_output_ml1 = output_ml1_to_dict(dates=dates, output_ml1=output_ml1[0,:].tolist(), precipitation=all_grided_data.tolist())
     #Convert output ml2 to dict
     dict_output_ml2 = output_ml2_to_dict(dates=dates[-input_size_ml2:],output_ml2=output_ml2)
-    # #convert the array to tiff
-    # filenametif = f"Prediction flood {start_run_pred}"
-    # filenametif = re.sub(r'[:,.-]', '', filenametif)
-    # convert_array_to_tif(data_array=output_ml2, filename=f"{filenametif}.tif")
     
     end_run_pred = get_current_datetime()
-    #timestamp_end = end_run_pred.strftime('%Y-%m-%d_%H-%M-%S')
     return start_run_pred, end_run_pred, dict_output_ml1, dict_output_ml2
 
 @app.post("/predict")
