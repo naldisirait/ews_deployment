851a8af54b6f0de90e80d8b41675fc220b2450d6
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-09-27 14:37:16 +0700

    asd

---
+++ test_ingest_data.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+import os
+import numpy as np
+import pickle
+import torch
+from datetime import datetime
+
+#import modul from this project
+from src.data_processing import get_input_ml1, get_input_hms
+from src.data_ingesting import get_prec_from_big_lake
+from src.utils import inference_model,to_tensor
+from src.post_processing import output_ml1_to_dict, output_ml2_to_dict
+
+#import model ml1 and ml2
+from models.discharge.model_ml1 import load_model_ml1
+from models.inundation.model_ml2  import load_model_ml2
+
+def get_current_datetime():
+    # Get the current date and time
+    now = datetime.now()
+    # Format the date and time as YYYY-MM-DD HH:MM:SS
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+
+def do_prediction():
+    cwd = os.getcwd()
+    start_run_pred = get_current_datetime()
+
+    #1. Define all constants and load models
+    hours_hms = 720
+    hours = 144
+    jumlah_subdas = 114
+    input_size_ml1 = hours * jumlah_subdas #jumlah jam dikali jumlah subdas
+    output_size_ml1 = 168 #jumlah debit yang diestimasi, 24 jam terakhir adalah hasil forecast
+    model_ml1 = load_model_ml1(input_size=input_size_ml1, output_size=output_size_ml1)
+
+    input_size_ml2 = 72
+    output_size_ml2 = 3078 * 2019 #rows x columns 
+    model_ml2 = load_model_ml2(input_size=input_size_ml2, output_size=output_size_ml2)
+
+    #2. Ingest Data input
+    path_config_stas_to_grid = "/opt/ews/ews_deployment/configs/configuration of stasiun to grid.json"
+    path_config_grid_to_subdas = "/opt/ews/ews_deployment/configs/configuration of grid to subdas.json"
+    #path_config_grid_to_df = "/opt/ews/ews_deployment/configs/configuration of grided to df.json"
+
+    ingested_data_name, ingested_data = get_prec_from_big_lake(hours)
+    #ingested_data_name_hms, ingested_data_hms = get_prec_from_big_lake(hours_hms)
+
+    #3.1 Inference ML1
+    all_grided_data, dates, input_ml1 =  get_input_ml1(ingested_data,
+                                                   ingested_data_name,
+                                                   path_config_stas_to_grid,
+                                                   path_config_grid_to_subdas)
+    print(type(input_ml1), input_ml1.shape)
+
+if __name__ == "__main__":
+    do_prediction()
