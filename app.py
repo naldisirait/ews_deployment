56b6fa250207de45b07b67002921ea1151497a85
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-09-27 20:48:08 +0700

    asd

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from typing import List
 import os
 from fastapi.encoders import jsonable_encoder
 import numpy as np
+import time
 import pickle
 import torch
 from datetime import datetime
 
 #import modul from this project
-from src.data_processing import get_input_ml1, get_input_hms
+from src.data_processing import get_input_ml1, get_input_hms, convert_prec_grided_to_ch_wilayah
 from src.data_ingesting import get_prec_from_big_lake
 from src.utils import inference_model, to_tensor
-from src.post_processing import output_ml1_to_dict, output_ml2_to_dict
+from src.post_processing import output_ml1_to_dict, output_ml2_to_dict, ensure_jsonable
 
 def get_input_debit_sample(name):
     try:
@@ -54,7 +55,7 @@ from models.inundation.model_ml2  import load_model_ml2
 
 app = FastAPI()
 def do_prediction():
-    cwd = os.getcwd()
+    tstart = time.time()
     start_run_pred = get_current_datetime()
 
     #1. Define all constants and load models
@@ -74,8 +75,10 @@ def do_prediction():
     path_config_grid_to_subdas = "/opt/ews/ews_deployment/configs/configuration of grid to subdas.json"
     #path_config_grid_to_df = "/opt/ews/ews_deployment/configs/configuration of grided to df.json"
 
-    ingested_data_name, ingested_data = get_prec_from_big_lake(hours)
+    ingested_data_name, ingested_data, runtime_ingest_data = get_prec_from_big_lake(hours)
     #ingested_data_name_hms, ingested_data_hms = get_prec_from_big_lake(hours_hms)
+    print(f"Ingested data runtime {runtime_ingest_data}")
+    print(f"ingested_data type {type(ingested_data)}")
 
     #3.1 Inference ML1
     all_grided_data, dates, input_ml1 =  get_input_ml1(ingested_data,
@@ -83,8 +86,12 @@ def do_prediction():
                                                    path_config_stas_to_grid,
                                                    path_config_grid_to_subdas)
     
+    print(f"Type of all_grided_data {type(all_grided_data)}")
+    print(f"Type of dates {type(all_grided_data)}")
+    print(f"Type of input_ml1 {type(input_ml1)}, shape input_ml1: {input_ml1.shape}")
+    
     output_ml1 = inference_model(model_ml1,input_ml1)
-    print(type(output_ml1), output_ml1.shape)
+    print(f"Type of output_ml1 {type(output_ml1)}, shape output_ml1: {output_ml1.shape}")
 
     #4.1 Inference ML2 using output from ML1
     output_ml1 = output_ml1[:,-input_size_ml2:]
@@ -93,41 +100,40 @@ def do_prediction():
     if dummy:
         output_ml1 = get_input_debit_sample(kasus)
     input_ml2 = np.expand_dims(output_ml1, axis=-1)
+
+    print(f"input_ml2 type: {type(input_ml2)}, shape: {input_ml2.shape}")
     input_ml2 = torch.tensor(input_ml2, dtype=torch.float32)
     output_ml2 = inference_model(model_ml2, input_ml2)
+    print(f"output_ml2 raw type: {type(output_ml2)}, shape: {output_ml2.shape}")
     output_ml2 = output_ml2[0,:].reshape(3078,2019)
-    print(output_ml2.shape)
+    print(f"output_ml2 after slicing and reshape type: {type(output_ml2)}, shape: {output_ml2.shape}")
 
-    #4.2 Inference ML2 using output from HMS
     #5. Bundle the Output
     #Convert output ml1 to dict
-    dates, dict_output_ml1 = output_ml1_to_dict(dates=dates, output_ml1=output_ml1[0,:].tolist(), precipitation=all_grided_data)
+    ch_wilayah = convert_prec_grided_to_ch_wilayah(all_grided_data)
+    dates, dict_output_ml1 = output_ml1_to_dict(dates=dates, output_ml1=output_ml1[0,:].tolist(), precipitation=ch_wilayah)
 
     #Convert output ml2 to dict
     dict_output_ml2 = output_ml2_to_dict(dates=dates[-input_size_ml2:],output_ml2=output_ml2)
-
-    for key,val in dict_output_ml1.items():
-        print(type(key), type(val))
-
-    for key,val in dict_output_ml2.items():
-        print(type(key), type(val))
     
     end_run_pred = get_current_datetime()
-    return start_run_pred, end_run_pred, dict_output_ml1, dict_output_ml2, output_ml1, output_ml2
+    tend = time.time()
+    prediction_runtime = tend-tstart
+
+    output = {"Prediction Time Start": str(start_run_pred), 
+              "Prediction time Finished": str(end_run_pred), 
+              "Prediction Output ml1": dict_output_ml1,
+              "Prediction Output ml2": dict_output_ml2}
+    
+    output = ensure_jsonable(output)
+
+    print(f"Prediction time {prediction_runtime}s")
+
+    return output
 
 @app.post("/predict")
 async def predict():
-    start_run_pred, end_run_pred, dict_output_ml1, dict_output_ml2, output_ml1, output_ml2 = do_prediction()
-    #return {"Output": outputs.tolist(),"Prediction Time": start_run_pred, "Prediction time Finished": end_run_pred}
-    # output = {"Prediction Time Start": str(start_run_pred), 
-    #           "Prediction time Finished": str(end_run_pred), 
-    #           "Prediction Output ml1": dict_output_ml1,
-    #           "Prediction Output ml2": dict_output_ml2}
-    
-    output = {"Prediction Output ml1": output_ml1[0,:].tolist(),
-            "Prediction Output ml2": output_ml2.tolist()}
-    
-    #json_compatible_output = jsonable_encoder(output)
+    output = do_prediction()
     return output
 
 #Local test
