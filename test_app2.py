fa3e4d9e2b029d19e8b760fb10f111770d477c30
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-10-10 16:55:27 +0700

    asd

--- test_app2.py
+++ test_app2.py
@@ -1,6 +1,11 @@
 from app2 import do_prediction
+from datetime import datetime, timedelta
 import os
-
+from src.data_ingesting_new import get_grided_prec_palu
 if __name__ == "__main__":
+
+    current_time = datetime(2023, 11, 20, 19, 0)
+    print("Try to ingest data gsmap")
+    val = get_grided_prec_palu(current_time)
     output = do_prediction()
     print("App2 berjalan dengan baik.")
