88c26ef20da3e8290524f967011e3f2d38b149cc
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-10-10 20:08:09 +0700

    asd

--- test_app2.py
+++ test_app2.py
@@ -4,7 +4,7 @@ import os
 from src.data_ingesting_new import get_grided_prec_palu
 if __name__ == "__main__":
 
-    current_time = datetime(2023, 11, 20, 19, 0)
+    current_time = datetime(2024, 8, 20, 19, 0)
     print("Try to ingest data gsmap")
     val = get_grided_prec_palu(current_time)
     output = do_prediction()
