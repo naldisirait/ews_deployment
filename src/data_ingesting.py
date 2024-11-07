1115407e446d18fc919e7d780fc3dd0d4b3438b8
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-11-07 17:11:54 +0700

    update

--- src/data_ingesting.py
+++ src/data_ingesting.py
@@ -242,4 +242,6 @@ def get_input_ml1_hujan_max():
     # Open the file in binary read mode and load the object
     with open(path_hujan_max, 'rb') as file:
         data = pickle.load(file)
-    return data
+        value = data['data_hujan_max']
+        value = torch.tensor(value, dtype=torch.float)
+    return value
